daab0f9e7559fbd864adaf391032c0ad109bf710
arnab7070 <arnabnandi7074@gmail.com>
2022-11-23 21:26:26 +0530

    Bottom Navigation Bar UI Changed

--- lib/widgets/b_nav.dart
+++ lib/widgets/b_nav.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sweet_nav_bar/sweet_nav_bar.dart';
-
+// import 'package:sweet_nav_bar/sweet_nav_bar.dart';
+import 'package:sliding_clipped_nav_bar/sliding_clipped_nav_bar.dart';
 import '../pages/switch_pages.dart';
 
 ValueNotifier<int> navIndexListener = ValueNotifier<int>(0);
@@ -22,44 +22,77 @@ class _BNavState extends State<BNav> {
     return ValueListenableBuilder<int>(
       valueListenable: navIndexListener,
       builder: (BuildContext context, int value, Widget? child) {
-        return SweetNavBar(
-          // paddingGradientColor: iconLinearGradiant,
-          currentIndex: navIndexListener.value,
-          // paddingBackgroundColor: ,
-          height: 10,
-          padding: EdgeInsets.zero,
-          borderRadius: 0,
-          items: [
-            SweetNavBarItem(
-              sweetActive: const Icon(Icons.home_rounded),
-              sweetIcon: const Icon(Icons.home_outlined),
-              sweetLabel: 'Home',
-              iconColors: iconLinearGradiant,
+        return SlidingClippedNavBar.colorful(
+          barItems: <BarItem>[
+            BarItem(
+              icon: Icons.home_rounded,
+              title: 'Home',
+              activeColor: Colors.blue,
+              inactiveColor: Colors.orange,
             ),
-            SweetNavBarItem(
-              sweetActive: const Icon(Icons.cloud),
-              sweetIcon: const Icon(Icons.cloud_queue_sharp),
-              sweetLabel: 'Web',
-              iconColors: iconLinearGradiant,
+            BarItem(
+              icon: Icons.public_rounded,
+              title: 'Web',
+              activeColor: Colors.pink,
+              inactiveColor: Colors.green,
             ),
-            SweetNavBarItem(
-              sweetActive: const Icon(Icons.sd_storage_rounded),
-              sweetIcon: const Icon(Icons.sd_storage_outlined),
-              sweetLabel: 'Local',
-              iconColors: iconLinearGradiant,
+            BarItem(
+              icon: Icons.storage_rounded,
+              title: 'Local',
+              activeColor: Colors.teal,
+              inactiveColor: Colors.red,
             ),
-            SweetNavBarItem(
-              sweetActive: const Icon(Icons.settings),
-              sweetIcon: const Icon(Icons.settings_outlined),
-              sweetLabel: 'Settings',
-              iconColors: iconLinearGradiant,
+            BarItem(
+              icon: Icons.settings,
+              title: 'Settings',
+              activeColor: Colors.cyan,
+              inactiveColor: Colors.deepPurple,
             ),
           ],
-          onTap: (index) {
+          selectedIndex: navIndexListener.value,
+          onButtonPressed: (index) {
             navIndexListener.value = index;
             Pages.currPageIndex.value = index;
           },
         );
+        // return SweetNavBar(
+        //   // paddingGradientColor: iconLinearGradiant,
+        //   currentIndex: navIndexListener.value,
+        //   // paddingBackgroundColor: ,
+        //   height: 10,
+        //   padding: EdgeInsets.zero,
+        //   borderRadius: 0,
+        //   items: [
+        //     SweetNavBarItem(
+        //       sweetActive: const Icon(Icons.home_rounded),
+        //       sweetIcon: const Icon(Icons.home_outlined),
+        //       sweetLabel: 'Home',
+        //       iconColors: iconLinearGradiant,
+        //     ),
+        //     SweetNavBarItem(
+        //       sweetActive: const Icon(Icons.cloud),
+        //       sweetIcon: const Icon(Icons.cloud_queue_sharp),
+        //       sweetLabel: 'Web',
+        //       iconColors: iconLinearGradiant,
+        //     ),
+        //     SweetNavBarItem(
+        //       sweetActive: const Icon(Icons.sd_storage_rounded),
+        //       sweetIcon: const Icon(Icons.sd_storage_outlined),
+        //       sweetLabel: 'Local',
+        //       iconColors: iconLinearGradiant,
+        //     ),
+        //     SweetNavBarItem(
+        //       sweetActive: const Icon(Icons.settings),
+        //       sweetIcon: const Icon(Icons.settings_outlined),
+        //       sweetLabel: 'Settings',
+        //       iconColors: iconLinearGradiant,
+        //     ),
+        //   ],
+        //   onTap: (index) {
+        //     navIndexListener.value = index;
+        //     Pages.currPageIndex.value = index;
+        //   },
+        // );
       },
     );
   }
