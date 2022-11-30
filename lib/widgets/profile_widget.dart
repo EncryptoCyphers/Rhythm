8fb0c0dc367edc431efb8917387d698a8fc04560
bishal <bishalkarmakar007@gmail.com>
2022-11-30 23:06:48 +0530

    minor

--- lib/widgets/profile_widget.dart
+++ lib/widgets/profile_widget.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import '../pages/userProfile.dart';
+import '../pages/user_profile.dart';
 
 var name = "", username = "", email = "";
 getData(String userEmail) {
