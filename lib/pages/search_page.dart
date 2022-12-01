6027b2f29af8fab973740b3db4102edf5caabd80
bishal <bishalkarmakar007@gmail.com>
2022-12-01 11:54:05 +0530

    Search Page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:music_player_app/pages/web.dart';
 import 'package:music_player_app/services/switch_pages.dart';
 import 'package:music_player_app/services/data_service_and_song_query.dart';
 import 'package:music_player_app/services/screen_sizes.dart';
@@ -10,19 +11,16 @@ import '../services/colours.dart';
 import '../widgets/drawer_menu.dart';
 //import '../widgets/bottomNavigationBar.dart';
 import 'mini_player.dart';
-import '../pages/search_page.dart';
+import 'package:searchbar_animation/searchbar_animation.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage({
-    required this.nm,
-  });
-  String nm;
+class SearchPage extends StatefulWidget {
+  const SearchPage();
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<SearchPage> createState() => _SearchPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _SearchPageState extends State<SearchPage> {
   final pageController = PageController(initialPage: 0);
   DateTime timeBackPressed = DateTime.now();
 
@@ -47,59 +45,45 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: fgPurple,
         /*centerTitle: true,
             title: const Text("RYTHM"),*/
-
         actions: [
           Padding(
-              padding: const EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SearchPage()));
-                },
-                child: const Icon(
-                  Icons.search,
-                  size: 26.0,
-                ),
-              )),
+            padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: SearchBarAnimation(
+                  textEditingController: _musicController,
+                  isOriginalAnimation: true,
+                  durationInMilliSeconds: 300,
+                  trailingWidget: const Icon(Icons.search),
+                  secondaryButtonWidget: const Icon(Icons.close),
+                  buttonWidget: const Icon(Icons.search),
+                  isSearchBoxOnRightSide: true,
+                  hintText: "Search Songs, Artists...",
+                  //hintTextColour: Colors.deepPurple,
+                  enableKeyboardFocus: true,
+                  onFieldSubmitted: (String value) {
+                    isSearchLoading.value = true;
+                    _dataService.getMusic(_musicController.text);
+                  }),
+            ),
+          ),
         ],
-      ),
-      drawer: DrawerMenu(
-        pageController: pageController,
       ),
       bottomNavigationBar: Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           const MiniPlayerWidget(),
-          ValueListenableBuilder<double>(
-            valueListenable: playerExpandProgress,
-            builder:
-                (BuildContext context, double minPlayerHeight, Widget? child) {
-              if (minPlayerHeight <= 100) {
-                // print('in');
-                // return const BottomNavBar();
-                return BNav(
-                  pageController: pageController,
-                );
-              } else {
-                return SizedBox(
-                  height: 0,
-                  width: logicalWidth,
-                );
-              }
-            },
+          SizedBox(
+            height: 0,
+            width: logicalWidth,
           ),
         ],
       ),
       backgroundColor: Colors.white,
 
       // Defined in switch_pages.dart
-      body: Pages(
-        nm: widget.nm,
-        pageController: pageController,
-      ),
+      body: Container(),
     );
   }
 }
