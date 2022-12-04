b94c3e9b002eba48b51d9db2063e8112a9adee0e
arnab7070 <arnabnandi7074@gmail.com>
2022-12-04 22:40:46 +0530

    On Tap Function Added on Carousel

--- lib/pages/web.dart
+++ lib/pages/web.dart
@@ -4,8 +4,13 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 //import 'package:getwidget/components/carousel/gf_carousel.dart';
 import 'package:getwidget/components/list_tile/gf_list_tile.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:music_player_app/pages/full_player.dart';
+import 'package:music_player_app/pages/mini_player.dart';
 import 'package:music_player_app/services/trending_songs.dart';
 import 'package:youtube/youtube_thumbnail.dart';
+
+import '../services/get_yt_searches.dart';
+import '../services/player_logic.dart';
 
 // ignore: must_be_immutable
 class Youtube extends StatefulWidget {
@@ -70,8 +75,11 @@ class _YoutubeState extends State<Youtube> {
                         enlargeCenterPage: true,
                       ),
                       items: trendingSongList.map((trendingSong) {
-                        return Image.network(
-                            YoutubeThumbnail(youtubeId: trendingSong.id).mq());
+                        return GestureDetector(
+                            onTap: () {},
+                            child: Image.network(
+                              YoutubeThumbnail(youtubeId: trendingSong.id).mq(),
+                            ));
                       }).toList(),
                     );
                   } else {
