e0d60df90d4dfcdac26f51fc13aef525ce7389d7
ANKIT <ankitpaul894@gmail.com>
2022-12-07 07:56:21 +0530

    changed trending song fetching logic

--- lib/services/json_object_model.dart
+++ lib/services/json_object_model.dart
@@ -1,59 +1,27 @@
-class SongList {
-  SongList({
-    required this.items,
-  });
-
-  List<Item> items;
-
-  factory SongList.fromJson(Map<String, dynamic> json) => SongList(
-        items: List<Item>.from(json["items"].map((x) => Item.fromJson(x))),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "items": List<dynamic>.from(items.map((x) => x.toJson())),
-      };
-}
-
-class Item {
-  Item({
-    required this.kind,
-    required this.etag,
-    required this.id,
-  });
-
-  String kind;
-  String etag;
-  Id id;
-
-  factory Item.fromJson(Map<String, dynamic> json) => Item(
-        kind: json["kind"],
-        etag: json["etag"],
-        id: Id.fromJson(json["id"]),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "kind": kind,
-        "etag": etag,
-        "id": id.toJson(),
-      };
-}
-
-class Id {
-  Id({
-    required this.kind,
+class TrendingSongs {
+  TrendingSongs({
+    required this.title,
     required this.videoId,
+    required this.author,
+    required this.lengthSeconds,
   });
 
-  String kind;
+  String title;
   String videoId;
+  String author;
+  int lengthSeconds;
 
-  factory Id.fromJson(Map<String, dynamic> json) => Id(
-        kind: json["kind"],
+  factory TrendingSongs.fromJson(Map<String, dynamic> json) => TrendingSongs(
+        title: json["title"],
         videoId: json["videoId"],
+        author: json["author"],
+        lengthSeconds: json["lengthSeconds"],
       );
 
   Map<String, dynamic> toJson() => {
-        "kind": kind,
+        "title": title,
         "videoId": videoId,
+        "author": author,
+        "lengthSeconds": lengthSeconds,
       };
 }
--- lib/services/trending_songs.dart
+++ lib/services/trending_songs.dart
@@ -1,5 +1,8 @@
-import 'package:youtube_data_api/youtube_data_api.dart';
-import 'package:youtube_data_api/models/video.dart';
+import 'package:music_player_app/services/json_object_model.dart';
+//import 'package:youtube_data_api/youtube_data_api.dart';
+//import 'package:youtube_data_api/models/video.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class TrendingMusic {
   // ignore: prefer_typing_uninitialized_variables
@@ -15,6 +18,7 @@ class TrendingMusic {
 List<TrendingMusic> trendingSongList = [];
 
 class Trending {
+  /*
   YoutubeDataApi youtubeDataApi = YoutubeDataApi();
   getTrendingMusic() async {
     List<Video> music = await youtubeDataApi.fetchTrendingMusic();
@@ -26,6 +30,31 @@ class Trending {
       trendingMusic.title = music[i].title;
       trendingMusic.artist = music[i].channelName;
       trendingMusic.duration = music[i].duration;
+      trendingSongList.add(trendingMusic);
+    }
+  }
+  */
+  getTrendingMusic() async {
+    const Map<String, String> headers = {
+      'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    };
+    final queryParameters = {'type': 'Music', 'region': 'IN'};
+    final uri =
+        Uri.https('invidious.snopyta.org', '/api/v1/trending', queryParameters);
+    final response = await http.get(uri, headers: headers);
+    //print(response.body);
+
+    var list = (json.decode(response.body) as List)
+        .map((data) => TrendingSongs.fromJson(data))
+        .toList();
+    trendingSongList = [];
+    for (int i = 0; i < list.length; i++) {
+      TrendingMusic trendingMusic = TrendingMusic();
+      trendingMusic.id = list[i].videoId;
+      trendingMusic.artist = list[i].author;
+      trendingMusic.title = list[i].title;
+      trendingMusic.duration = list[i].lengthSeconds;
       trendingSongList.add(trendingMusic);
     }
   }
