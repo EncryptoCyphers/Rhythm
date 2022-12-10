0935617f4e6309df4b02ee262889465f408182f4
bishal <bishalkarmakar007@gmail.com>
2022-12-10 21:30:45 +0530

    skip Gestures on MiniPlayer

--- lib/widgets/circular_mini_player.dart
+++ lib/widgets/circular_mini_player.dart
@@ -8,7 +8,72 @@ import 'package:page_animation_transition/page_animation_transition.dart';
 import '../pages/mini_player.dart';
 import '../services/player_logic.dart';
 
+int horizontalSwipeVariable = 0;
 EdgeInsets miniPlayerPadding = const EdgeInsets.all(0);
+
+void skipToPrev() {
+  if (currSongIndex >= 0 &&
+      currSongIndex < currSongList!.length &&
+      currSongList!.length > 1) {
+    // print(currSongIndex);
+
+    currSongIndex--;
+    if (currSongIsWeb) {
+      currSongIndexListenable.value = currSongIndex;
+      fetchSongUriForCurrList(currSongIndex);
+      // setState(() {
+      //   currSongList![currSongIndex].title =
+      //       currSongList![currSongIndex].title;
+      // });
+    } else {
+      getCurrSongInfo(
+        id: currSongList![currSongIndex].id.toString(),
+        uri: currSongList![currSongIndex].uri,
+        duration: currSongIsWeb
+            ? (currSongList![currSongIndex].duration)
+            : (Duration(milliseconds: currSongList![currSongIndex].duration)),
+        isWeb: currSongList![currSongIndex].isWeb,
+        name: currSongList![currSongIndex].title,
+        artist: currSongList![currSongIndex].artist.toString(),
+        songIndex: currSongIndex,
+      );
+      currSongIdListenable.value = currSongList![currSongIndex].id.toString();
+      playSong(audioPlayer: audioPlayer);
+    }
+  }
+}
+
+void skipToNext() {
+  if (currSongIndex >= 0 &&
+      currSongIndex < currSongList!.length &&
+      currSongList!.length > 1) {
+    // print(currSongIndex);
+
+    currSongIndex++;
+    if (currSongIsWeb) {
+      currSongIndexListenable.value = currSongIndex;
+      fetchSongUriForCurrList(currSongIndex);
+      // setState(() {
+      //   currSongList![currSongIndex].title =
+      //       currSongList![currSongIndex].title;
+      // });
+    } else {
+      getCurrSongInfo(
+        id: currSongList![currSongIndex].id.toString(),
+        uri: currSongList![currSongIndex].uri,
+        duration: currSongIsWeb
+            ? (currSongList![currSongIndex].duration)
+            : (Duration(milliseconds: currSongList![currSongIndex].duration)),
+        isWeb: currSongList![currSongIndex].isWeb,
+        name: currSongList![currSongIndex].title,
+        artist: currSongList![currSongIndex].artist.toString(),
+        songIndex: currSongIndex,
+      );
+      currSongIdListenable.value = currSongList![currSongIndex].id.toString();
+      playSong(audioPlayer: audioPlayer);
+    }
+  }
+}
 
 class CircularMiniPlayer extends StatefulWidget {
   const CircularMiniPlayer({super.key});
@@ -36,65 +101,69 @@ class _CircularMiniPlayerState extends State<CircularMiniPlayer> {
                   //       const EdgeInsets.fromLTRB(0, 0, 0, 0);
                   // });
                   return GestureDetector(
-                    onPanUpdate: (details) {
-                      int sensitivity = 5;
-                      if (details.delta.dy < sensitivity) {
-                        // Future.delayed(Duration(milliseconds: 50), () {
-                        Navigator.of(context).push(
-                          PageAnimationTransition(
-                              page: const Player(),
-                              pageAnimationType: BottomToTopTransition()),
-                        );
-                        // });
-                      } else if (details.delta.dy > 0) {
-                        isFetchingUri.value = false;
-                        audioPlayer.stop();
-                        bNavPaddingListenable.value =
-                            const EdgeInsets.fromLTRB(0, 0, 45, 0);
-                        miniPlayerVisibilityListenable.value = false;
-                      }
-                      // if (details.delta.dx > 0) {
-                      //   () {
-                      //     if (currSongIndex >= 0 &&
-                      //         currSongIndex < currSongList!.length &&
-                      //         currSongList!.length > 1) {
-                      //       // print(currSongIndex);
-
-                      //       currSongIndex++;
-                      //       if (currSongIsWeb) {
-                      //         fetchSongUriForCurrList(currSongIndex);
-                      //         // setState(() {
-                      //         //   currSongList![currSongIndex].title =
-                      //         //       currSongList![currSongIndex].title;
-                      //         // });
-                      //       } else {
-                      //         getCurrSongInfo(
-                      //           id: currSongList![currSongIndex].id.toString(),
-                      //           uri: currSongList![currSongIndex].uri,
-                      //           duration: currSongIsWeb
-                      //               ? (currSongList![currSongIndex].duration)
-                      //               : (Duration(
-                      //                   milliseconds:
-                      //                       currSongList![currSongIndex].duration)),
-                      //           isWeb: currSongList![currSongIndex].isWeb,
-                      //           name: currSongList![currSongIndex].title,
-                      //           artist:
-                      //               currSongList![currSongIndex].artist.toString(),
-                      //           songIndex: currSongIndex,
-                      //         );
-                      //         currSongIdListenable.value =
-                      //             currSongList![currSongIndex].id.toString();
-                      //         playSong(audioPlayer: audioPlayer);
-                      //       }
-                      //     }
-                      //   };
-                      // } else if (details.delta.dx < -sensitivity) {
-                      //   // audioPlayer.stop();
-                      //   // bNavPaddingListenable.value =
-                      //   //     const EdgeInsets.fromLTRB(0, 0, 45, 0);
-                      //   // miniPlayerVisibilityListenable.value = false;
-                      // }
+                    onPanStart: (details) {
+                      horizontalSwipeVariable = 0;
                     },
+                    onPanUpdate: (details) {
+                      if (details.delta.dy.abs() > details.delta.dx.abs()) {
+                        //print("Vertical");
+                        if (details.delta.dy > 3) {
+                          //print("U -> D");
+                          isFetchingUri.value = false;
+                          audioPlayer.stop();
+                          bNavPaddingListenable.value =
+                              const EdgeInsets.fromLTRB(0, 0, 45, 0);
+                          miniPlayerVisibilityListenable.value = false;
+                        } else if (details.delta.dy < -3) {
+                          //print("D -> U");
+                          Navigator.of(context).push(
+                            PageAnimationTransition(
+                                page: const Player(),
+                                pageAnimationType: BottomToTopTransition()),
+                          );
+                        }
+                      } else if (details.delta.dy.abs() <
+                          details.delta.dx.abs()) {
+                        //   //print("Horizontal");
+                        if (details.delta.dx < -2) {
+                          //print("R -> L");
+                          horizontalSwipeVariable = -1;
+                        } else if (details.delta.dx > 2) {
+                          //print("L -> R");
+                          horizontalSwipeVariable = 1;
+                        }
+                      }
+                    },
+                    onPanEnd: (details) {
+                      if (horizontalSwipeVariable == -1) {
+                        skipToPrev();
+                        // print("R -> L");
+                        horizontalSwipeVariable = 0;
+                      } else if (horizontalSwipeVariable == 1) {
+                        skipToNext();
+                        // print("L -> R");
+                        horizontalSwipeVariable = 0;
+                      }
+                    },
+
+                    // onPanUpdate: (details) {
+                    //   int sensitivity = 5;
+                    //   if (details.delta.dy < sensitivity) {
+                    //     // Future.delayed(Duration(milliseconds: 50), () {
+                    //     Navigator.of(context).push(
+                    //       PageAnimationTransition(
+                    //           page: const Player(),
+                    //           pageAnimationType: BottomToTopTransition()),
+                    //     );
+                    //     // });
+                    //   } else if (details.delta.dy > 0) {
+                    //     isFetchingUri.value = false;
+                    //     audioPlayer.stop();
+                    //     bNavPaddingListenable.value =
+                    //         const EdgeInsets.fromLTRB(0, 0, 45, 0);
+                    //     miniPlayerVisibilityListenable.value = false;
+                    //   }
+                    // },
                     onTap: () {
                       if (isPlaying) {
                         isPlayingListenable.value = false;
@@ -105,49 +174,9 @@ class _CircularMiniPlayerState extends State<CircularMiniPlayer> {
                       }
                       isPlaying = !isPlaying;
                     },
-                    onDoubleTap: () {
-                      if (currSongIndex >= 0 &&
-                          currSongIndex < currSongList!.length &&
-                          currSongList!.length > 1) {
-                        // print(currSongIndex);
-
-                        currSongIndex++;
-                        if (currSongIsWeb) {
-                          currSongIndexListenable.value = currSongIndex;
-                          fetchSongUriForCurrList(currSongIndex);
-                          // setState(() {
-                          //   currSongList![currSongIndex].title =
-                          //       currSongList![currSongIndex].title;
-                          // });
-                        } else {
-                          getCurrSongInfo(
-                            id: currSongList![currSongIndex].id.toString(),
-                            uri: currSongList![currSongIndex].uri,
-                            duration: currSongIsWeb
-                                ? (currSongList![currSongIndex].duration)
-                                : (Duration(
-                                    milliseconds:
-                                        currSongList![currSongIndex].duration)),
-                            isWeb: currSongList![currSongIndex].isWeb,
-                            name: currSongList![currSongIndex].title,
-                            artist:
-                                currSongList![currSongIndex].artist.toString(),
-                            songIndex: currSongIndex,
-                          );
-                          currSongIdListenable.value =
-                              currSongList![currSongIndex].id.toString();
-                          playSong(audioPlayer: audioPlayer);
-                        }
-                      }
-                    },
-                    // : ,
-                    // onTap: () {
-                    //   Navigator.of(context).push(
-                    //     PageAnimationTransition(
-                    //         page: const Player(),
-                    //         pageAnimationType: BottomToTopTransition()),
-                    //   );
-                    // },
+                    //onDoubleTap: () {
+                    // skipToNext();
+                    //},
                     child: const MiniArtWork(),
                   );
                 } else {
